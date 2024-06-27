3172b1502fc8587e166ce63bd4bda57f53bda060
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-06-27 17:28:33 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.set_page_config(layout='wide',
+                   page_title='Banco X',
+                   page_icon='https://png.pngtree.com/png-vector/20211030/ourmid/pngtree-letter-x-logo-png-image_3990472.png')
