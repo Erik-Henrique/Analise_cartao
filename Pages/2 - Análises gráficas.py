3aed6be1f0fac685ba9f74ae221d7845108df63b
Erik Henrique Dias Ferreira <150151321+Erik-Henrique@users.noreply.github.com>
2024-06-28 09:27:13 -0300

    Update 2 - Análises gráficas.py

--- Pages/2 - Análises gráficas.py
+++ Pages/2 - Análises gráficas.py
@@ -5,12 +5,6 @@ import matplotlib.pyplot as plt
 
 def app():
 #
-
-  st.set_page_config(layout='wide',
-                     page_title='Banco X',
-                     page_icon='https://png.pngtree.com/png-vector/20211030/ourmid/pngtree-letter-x-logo-png-image_3990472.png')
-  
-  
   page_bg_img = """
       <style>
       [data-testid="stAppViewContainer"] {
